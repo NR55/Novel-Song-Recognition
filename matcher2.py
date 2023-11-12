f2645a0932f442fd840c340c225addc0d033d13b
NR55 <nandan55ramesh@gmail.com>
2023-11-12 20:50:21 +0530

    Minor Tweaks

--- matcher2.py
+++ matcher2.py
@@ -5,57 +5,47 @@ import os
 import numpy as np
 import sounddevice as sd
 import pickle
-from preprocessing import create_constellation, create_hashes  # Import preprocessing functions
+from preprocessing import constel_gen, hash_gen  # Import preprocessing functions
+
+background_img_path = "./AppBackground/Music_Wallpaper2.jpg"
+default_img_path="./CoverImages/Default.jpg"
 
 class SongRecognitionApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Song Recognition App")
 
-        # Load the background image
-        image_path = "./AppBackground/Music_Wallpaper1.jpg"  # Replace with the actual path to your image
-        background_image = Image.open(image_path)
+        background_image = Image.open(background_img_path)
 
-        # Set the size of the Tkinter window to match the image
         self.root.geometry(f"{background_image.width}x{background_image.height}")
 
-        # Create a label to display the background image
         background_photo = ImageTk.PhotoImage(background_image)
         background_label = tk.Label(root, image=background_photo)
         background_label.image = background_photo
         background_label.place(relx=0.5, rely=0.5, anchor="center")
 
-        # Create a style for the record button
         style = ttk.Style()
         style.configure("TButton", font=("Helvetica", 14))
 
-        # Add the record button and place it in the center
         self.record_button = ttk.Button(root, text="Record Audio", command=self.record_and_recognize, style="TButton")
         self.record_button.place(relx=0.5, rely=0.8, anchor="center")
 
-        # Add the result label and place it in the center with rounded corners and a custom background color
-        self.result_label = tk.Label(root, text="", font=("Helvetica", 16), bg="#FF5733", fg="black", borderwidth=2, relief="solid", padx=10, pady=5, anchor="center")
+        self.result_label = tk.Label(root, text="", font=("Helvetica", 16), bg="#808080", fg="white", borderwidth=2, relief="solid", padx=10, pady=5, anchor="center")
         self.result_label.place(relx=0.5, rely=0.9, anchor="center")
 
-        # Load the database
         self.database = pickle.load(open('./Database/database.pickle', 'rb'))
         self.song_name_index = pickle.load(open("./Database/song_index.pickle", "rb"))
 
-        # Load the folder containing song cover images
         self.cover_images_folder = "./CoverImages"
 
     def record_and_recognize(self):
-        # Record audio from the microphone
-        duration = 10  # Set the duration of recording (in seconds)
+        duration = 10
         Fs, audio_input = self.record_audio(duration)
 
-        # Remove noise from the recorded audio
         audio_input = self.remove_noise(audio_input)
 
-        # Perform audio matching with the database and score against the database
         result = self.audio_matching(audio_input, Fs, threshold=0.00)
 
-        # Display the result on the GUI
         self.display_result(result)
 
     def record_audio(self, duration=10, fs=44100):
@@ -66,7 +56,6 @@ class SongRecognitionApp:
         return fs, audio_input.flatten()
 
     def remove_noise(self, audio_signal, threshold=0.00):
-        # Apply thresholding to remove noise
         audio_signal[np.abs(audio_signal) < threshold] = 0.0
         return audio_signal
 
@@ -79,8 +68,6 @@ class SongRecognitionApp:
                     if song_index not in matches_per_song:
                         matches_per_song[song_index] = []
                     matches_per_song[song_index].append((hash, sample_time, source_time))
-
-        # Calculate and visualize matching scores for the recorded audio
         scores = {}
         for song_index, matches in matches_per_song.items():
             song_scores_by_offset = {}
@@ -94,53 +81,34 @@ class SongRecognitionApp:
             max_score = song_scores_by_offset[max_offset]
 
             scores[song_index] = (max_offset, max_score)
-
-        # Sort the scores for better readability
         sorted_scores = list(sorted(scores.items(), key=lambda x: x[1][1], reverse=True))
-
         return sorted_scores
 
     def audio_matching(self, audio_input, Fs, threshold=0.00):
-        # Create the constellation and hashes
-        constellation = create_constellation(audio_input, Fs)
-        hashes = create_hashes(constellation, None)
-
-        # Call the function to score hashes against the database
+        constellation = constel_gen(audio_input, Fs)
+        hashes = hash_gen(constellation, None)
         scores = self.score_hashes_against_database(hashes)
-
-        # Print the top matching songs from the audio input
         result = []
         for song_index, (offset, score) in scores[:5]:
             result.append((self.song_name_index[song_index], score))
-
         return result
 
     def display_result(self, result):
-        # Get the top-matched song and its score
         top_song, score = result[0]
-
-        # Extract the filename without the path or extension
         song_filename = os.path.splitext(os.path.basename(top_song))[0]
-
-        # Update the result label with the top-matched song and score
-        self.result_label.config(text=f"Top matching song: {song_filename} (Score: {score})")
-
-        # Load and display the cover image for the top-matched song
+        self.result_label.config(text=f"Best-matching Song : {song_filename}")
         cover_image_path = os.path.join(self.cover_images_folder, f"{song_filename}.jpeg")
         print(f"Cover image path: {cover_image_path}")
-
         if os.path.exists(cover_image_path):
             cover_image = Image.open(cover_image_path)
             cover_photo = ImageTk.PhotoImage(cover_image)
-
         else:
-            cover_image = Image.open("./CoverImages/Default.jpg")
+            cover_image = Image.open(default_img_path)
             cover_photo = ImageTk.PhotoImage(cover_image)
-        
-        # Create a label to display the cover image
+
         cover_label = tk.Label(self.root, image=cover_photo)
         cover_label.image = cover_photo
-        cover_label.place(relx=0.5, rely=0.6, anchor="center")
+        cover_label.place(relx=0.5, rely=0.55, anchor="center")
 
 
 if __name__ == "__main__":
--- preprocessing.py
+++ preprocessing.py
@@ -10,23 +10,19 @@ import glob
 
 folder_path = "Songs"
 
-def create_constellation(audio, Fs):
-    # Parameters
+def constel_gen(audio, Fs):
+
     window_length_seconds = 0.5
     window_length_samples = int(window_length_seconds * Fs)
     window_length_samples += window_length_samples % 2
     num_peaks = 15
-
-    # Pad the song to divide evenly into windows
     amount_to_pad = window_length_samples - audio.size % window_length_samples
     song_input = np.pad(audio, (0, amount_to_pad))
-
-    # Perform a short time Fourier transform
     frequencies, times, stft = signal.stft(
         song_input, Fs, nperseg=window_length_samples, nfft=window_length_samples, return_onesided=True
     )
 
-    constellation_map = []
+    conste_map = []
 
     for time_idx, window in enumerate(stft.T):
         spectrum = abs(window)
@@ -35,75 +31,53 @@ def create_constellation(audio, Fs):
         largest_peaks = np.argpartition(props["prominences"], -n_peaks)[-n_peaks:]
         for peak in peaks[largest_peaks]:
             frequency = frequencies[peak]
-            constellation_map.append([time_idx, frequency])
+            conste_map.append([time_idx, frequency])
 
-    return constellation_map
+    return conste_map
 
-def create_hashes(constellation_map, song_id=None):
+def hash_gen(conste_map, song_id=None):
     hashes = {}
-    # Use this for binning - 23_000 is slightly higher than the maximum
-    # frequency that can be stored in the .wav files, 22.05 kHz
     upper_frequency = 23_000 
     frequency_bits = 10
-
-    # Iterate the constellation
-    for idx, (time, freq) in enumerate(constellation_map):
-        # Iterate the next 100 pairs to produce the combinatorial hashes
-        # When we produced the constellation before, it was sorted by time already
-        # So this finds the next n points in time (though they might occur at the same time)
-        for other_time, other_freq in constellation_map[idx : idx + 100]: 
+    for idx, (time, freq) in enumerate(conste_map):
+        for other_time, other_freq in conste_map[idx : idx + 100]: 
             diff = other_time - time
-            # If the time difference between the pairs is too small or large
-            # ignore this set of pairs
             if diff <= 1 or diff > 10:
                 continue
-
-            # Place the frequencies (in Hz) into 1024 bins
             freq_binned = freq / upper_frequency * (2 ** frequency_bits)
             other_freq_binned = other_freq / upper_frequency * (2 ** frequency_bits)
-
-            # Produce a 32-bit hash
-            # Use bit shifting to move the bits to the correct location
             hash_val = int(freq_binned) | (int(other_freq_binned) << 10) | (int(diff) << 20)
             hashes[hash_val] = (time, song_id)
     return hashes
 
-def plot_fft_with_stft_constellation_hashes_and_database(folder_path):
-    # Create a directory to store the FFT, STFT, constellation, hash, and database images
+def generator(folder_path):
     output_folder = "Database"
     os.makedirs(output_folder, exist_ok=True)
 
-    # Song Database
     song_name_index = {}
     database = {}
-
-    # Go through each song, using where they are alphabetically as an id
     songs = glob.glob(os.path.join(folder_path, '*.mp3'))
+    
     for index, filename in enumerate(tqdm(sorted(songs))):
+
         song_name_index[index] = filename
-        # Load audio file
         y, sr = librosa.load(filename)
 
-        # Create Constellation Map
-        constellation_map = create_constellation(y, sr)
-
-        # Create and Investigate Hashes
-        hashes = create_hashes(constellation_map, index)
+        conste_map = constel_gen(y, sr)
+        hashes = hash_gen(conste_map, index)
         for i, (hash_val, (time, _)) in enumerate(hashes.items()):
             if i > 10:
                 break
 
-        # Update the Database
         for hash_val, time_index_pair in hashes.items():
             if hash_val not in database:
                 database[hash_val] = []
             database[hash_val].append(time_index_pair)
 
-    # Dump the database and list of songs as pickles
     with open(os.path.join(output_folder, "database.pickle"), 'wb') as db:
         pickle.dump(database, db, pickle.HIGHEST_PROTOCOL)
     with open(os.path.join(output_folder, "song_index.pickle"), 'wb') as songs:
         pickle.dump(song_name_index, songs, pickle.HIGHEST_PROTOCOL)
 
 if __name__ == "__main__":
-    plot_fft_with_stft_constellation_hashes_and_database(folder_path)
+    generator(folder_path)
