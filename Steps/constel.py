c5f4ed66933a164ced24a3af5a1e1bdb38e34c48
NR55 <nandan55ramesh@gmail.com>
2023-11-13 09:39:01 +0530

    Steps Final Update

--- Steps/constel.py
+++ Steps/constel.py
@@ -1,55 +1,56 @@
-import os
 import numpy as np
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
-from scipy.signal import find_peaks
+from scipy import signal
+from IPython.display import Audio, display
+import sounddevice as sd
 
-def plot_stft(folder_path):
-    # Create a directory to store the STFT images
-    output_folder = os.path.join(folder_path, "stft_plots")
-    os.makedirs(output_folder, exist_ok=True)
+def plot_sound_wave_and_stft():
 
-    # Parameters for STFT
-    n_fft = 2048
-    hop_length = 512
+    example_file = librosa.example('trumpet')
 
-    # Iterate over all files in the folder
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".mp3"):
-            file_path = os.path.join(folder_path, filename)
+    y, sr = librosa.load(example_file)
 
-            # Load audio file
-            y, sr = librosa.load(file_path)
+    # Plot the sound wave
+    plt.figure(figsize=(6, 4))
+    librosa.display.waveshow(y, sr=sr)
+    plt.title('Sound Wave for Example Song')
+    plt.show()
 
-            # Compute Short-Time Fourier Transform (STFT)
-            D = librosa.amplitude_to_db(np.abs(librosa.stft(y, n_fft=n_fft, hop_length=hop_length)), ref=np.max)
+    # Perform STFT
+    D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
 
-            # Extract time and frequency information
-            times = librosa.times_like(D)
-            frequencies = librosa.fft_frequencies(sr=sr, n_fft=n_fft)
+    # Plot the STFT
+    plt.figure(figsize=(6, 4))
+    librosa.display.specshow(D, sr=sr, x_axis='time', y_axis='log')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('STFT for Example Song')
+    plt.show()
 
-            # Find peaks in each time frame
-            all_peaks = []
-            for frame in range(D.shape[1]):
-                magnitude = D[:, frame]
-                peaks, _ = find_peaks(magnitude, prominence=0, distance=100)
-                all_peaks.append(peaks)
+    # Perform constellation map
+    constellation_map = []
+    stft = librosa.stft(y)
+    frequencies = librosa.fft_frequencies(sr=sr)
+    
+    for time_idx, window in enumerate(stft.T):
+        spectrum = np.abs(window)
+        peaks, props = signal.find_peaks(spectrum, prominence=0, distance=200)
+        n_peaks = min(5, len(peaks))  # Ensure n_peaks is within bounds
+        largest_peaks = np.argpartition(props["prominences"], -n_peaks)[-n_peaks:]
+        for peak in peaks[largest_peaks]:
+            frequency = frequencies[peak]
+            constellation_map.append([time_idx, frequency])
 
-            # Flatten the list of peak indices
-            flattened_peaks = [peak for sublist in all_peaks for peak in sublist]
+    plt.scatter(*zip(*constellation_map))
+    plt.title('Constellation Map for Example Song')
+    plt.xlabel('Time Index')
+    plt.ylabel('Frequency (Hz)')
+    plt.show()
 
-            # Plot the STFT with identified peaks
-            plt.figure(figsize=(12, 6))
-            librosa.display.specshow(D, sr=sr, x_axis='time', y_axis='log')
-            plt.colorbar(format='%+2.0f dB')
-            plt.scatter(times[flattened_peaks], frequencies[flattened_peaks], color='r', s=5)
-            plt.title('STFT with Peaks for {}'.format(filename))
-            plt.xlabel('Time (s)')
-            plt.ylabel('Frequency (Hz)')
-            #plt.savefig(os.path.join(output_folder, filename.replace(".mp3", "_stft_plot.png")))
-            plt.show()
+    sd.play(y, sr)
+    sd.wait()
+
 
 if __name__ == "__main__":
-    folder_path = "../Songs"
-    plot_stft(folder_path)
+    plot_sound_wave_and_stft()
--- Steps/stft.py
+++ Steps/stft.py
@@ -3,7 +3,7 @@ import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 
-def plot_sound_wave():
+def plot_sound_wave_and_stft():
 
     example_file = librosa.example('trumpet')
 
@@ -15,5 +15,15 @@ def plot_sound_wave():
     plt.title('Sound Wave for Example Song')
     plt.show()
 
+    # Perform STFT
+    D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
+
+    # Plot the STFT
+    plt.figure(figsize=(12, 4))
+    librosa.display.specshow(D, sr=sr, x_axis='time', y_axis='log')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('STFT for Example Song')
+    plt.show()
+
 if __name__ == "__main__":
-    plot_sound_wave()
+    plot_sound_wave_and_stft()
